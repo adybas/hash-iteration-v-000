8204a3deabdd14f70b0cf73a132356c3435f9523
Anna Dybas <a.m.dybas@gmail.com>
2019-05-24 00:43:07 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,10 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name == name.start_with?("A")
-      puts "#{name}"
+      winner = name
     end
   end
 end
